cd09af833d53a64403990ada7957cf9f79875567
ingangi <huahui_c@hotmail.com>
2019-04-15 14:35:49 +0800

    larger pic buff

--- controllers/default.go
+++ controllers/default.go
@@ -44,7 +44,7 @@ func (c *MainController) Show() {
 	}
 
 	defer ff.Close()
-	buffer := make([]byte, 500000)
+	buffer := make([]byte, 1000000)
 	n, _ := ff.Read(buffer)
 	c.Ctx.Output.Body(buffer[:n])
 }
